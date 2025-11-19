9d8c6124f6fe0ec282c772010dcd0176c8ad0508
Arunesh Mishra <aruneshm@gmail.com>
2025-11-18 16:19:23 -0800

    Initial Commit

---
+++ TalkLens/TalkLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  TalkLensApp.swift
+//  TalkLens
+//
+//  Created by Arun Mishra on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TalkLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
